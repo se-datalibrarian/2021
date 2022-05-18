71691e8701045a474e81a3f5cc9f710beac277b4
gwenexner <49045733+gwenexner@users.noreply.github.com>
2022-05-18 12:39:13 -0400

    copying live to archive
    Copying all the files from the 2021 live website, prior to the changes for 2022

--- gridsome.config.js
+++ gridsome.config.js
@@ -25,18 +25,18 @@ module.exports = {
   siteDescription: `Website for the 2021 Southeast Data Librarian Symposium (SEDLS). The SEDLS is intended to provide a low-cost opportunity for librarians and other research data specialists to gather and explore developments in the field of data librarianship, including the management and sharing of research data.`,
   author: `@SEDataLibrarian`,
   siteUrl: 'https://se-datalibrarian.github.io',
-  pathPrefix: '/2021',
+  // pathPrefix: '/2021',
   plugins: [
-    // {
-    //   // This plugin sets up gtag to track analytics with Google Analytics
-    //   use: 'gridsome-plugin-gtag',
-    //   options: {
-    //       config: {
-    //         // Use GTAG_ID in .env file (GTAG_ID=G-XXXXXXXXXX) or set blank if not present
-    //           id: process.env.GTAG_ID || '',
-    //       },
-    //   },
-    // },
+    {
+      // This plugin sets up gtag to track analytics with Google Analytics
+      use: 'gridsome-plugin-gtag',
+      options: {
+          config: {
+            // Use GTAG_ID in .env file (GTAG_ID=G-XXXXXXXXXX) or set blank if not present
+              id: process.env.GTAG_ID || '',
+          },
+      },
+    },
     {
       use: 'gridsome-plugin-tailwindcss'
     },
